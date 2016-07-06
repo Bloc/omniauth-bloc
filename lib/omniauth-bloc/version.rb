4660e297689efcff3f3f5e02431b62edf22c60b5
Ariel Fogel <fogeltine@gmail.com>
2016-07-06 13:21:58 -0700

    allow oauth to login from a staging server

--- lib/omniauth-bloc/version.rb
+++ lib/omniauth-bloc/version.rb
@@ -1,5 +1,5 @@
 module OmniAuth
   module Bloc
-    VERSION = "1.1.0"
+    VERSION = "1.1.1"
   end
 end
--- lib/omniauth/strategies/bloc.rb
+++ lib/omniauth/strategies/bloc.rb
@@ -32,7 +32,38 @@ module OmniAuth
         # Fixes regression in omniauth-oauth2 v1.4.0 by https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
         options[:callback_url] || (full_host + script_name + callback_path)
       end
+    end
 
+    class BlocStagingTwo < OmniAuth::Strategies::OAuth2
+      option :name, 'bloc_staging2'
+
+      option :client_options, {
+               authorize_url: '/oauth/authorize',
+               site: 'https://staging2.bloc.io'
+             }
+
+      uid { raw_info["id"].to_s }
+
+      info do
+        {
+          email: raw_info["email"],
+          username: raw_info["name"],
+          name: raw_info["name"],
+          bio: raw_info["bio"],
+          user_type: raw_info["type"],
+          status: raw_info["status"],
+          image_url: raw_info["image_src"]
+        }
+      end
+
+      def raw_info
+        @raw_info ||= access_token.get('/api/v1/me.json').parsed
+      end
+
+      def callback_url
+        # Fixes regression in omniauth-oauth2 v1.4.0 by https://github.com/intridea/omniauth-oauth2/commit/85fdbe117c2a4400d001a6368cc359d88f40abc7
+        options[:callback_url] || (full_host + script_name + callback_path)
+      end
     end
   end
 end
